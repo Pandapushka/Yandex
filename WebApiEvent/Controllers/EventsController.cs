599ff2fd5dd5efc987e271d84d13f0520f4bf204
Никита Андреев <andreev.nik.d@yandex.ru>
2026-04-27 13:56:23 +0300

    Поправил контроллер под GlobalExceptionHandlingMiddleware

--- WebApiEvent/Controllers/EventsController.cs
+++ WebApiEvent/Controllers/EventsController.cs
@@ -13,15 +13,8 @@ namespace WebApiEvent.Controllers
         [HttpGet]
         public ActionResult<ResponseServerDto<List<EventDtoResponse>>> GetAll()
         {
-            try
-            {
-                var result = _eventService.GetAll();
-                return Ok(ResponseServerDto<List<EventDtoResponse>>.Success(result, 200));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ResponseServerDto<List<EventDtoResponse>>.Error(ex.Message, 500));
-            }
+            var result = _eventService.GetAll();
+            return Ok(ResponseServerDto<List<EventDtoResponse>>.Success(result, 200));
         }
 
         [HttpGet("{id:guid}")]
@@ -34,77 +27,29 @@ namespace WebApiEvent.Controllers
         [HttpPost]
         public ActionResult<ResponseServerDto<string>> Create([FromBody] EventDtoRequest request)
         {
-            try
-            {
-                var id = _eventService.Create(request);
-                return CreatedAtAction(nameof(GetById), new { id }, ResponseServerDto<string>.Success($"Событие c id {id}  успешно создано", 201));
-            }
-            catch (CustomValidationException ex)
-            {
-                return BadRequest(ResponseServerDto<string>.Error(ex.Message, 400));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ResponseServerDto<string>.Error(ex.Message, 500));
-            }
+            var id = _eventService.Create(request);
+            return CreatedAtAction(nameof(GetById), new { id }, ResponseServerDto<string>.Success($"Событие c id {id} успешно создано", 201));
         }
 
         [HttpPut("{id:guid}")]
         public ActionResult<ResponseServerDto<string>> Update(Guid id, [FromBody] UpdateEventDtoRequest request)
         {
-            try
-            {
-                _eventService.Update(id, request);
-                return Ok(ResponseServerDto<string>.Success("Событие успешно обновлено", 200));
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ResponseServerDto<string>.Error(ex.Message, 404));
-            }
-            catch (CustomValidationException ex)
-            {
-                return BadRequest(ResponseServerDto<string>.Error(ex.Message, 400));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ResponseServerDto<string>.Error(ex.Message, 500));
-            }
+            _eventService.Update(id, request);
+            return Ok(ResponseServerDto<string>.Success("Событие успешно обновлено", 200));
         }
 
         [HttpDelete("{id:guid}")]
         public ActionResult<ResponseServerDto<string>> Delete(Guid id)
         {
-            try
-            {
-                _eventService.Delete(id);
-                return Ok(ResponseServerDto<string>.Success("Событие успешно удалено", 200));
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ResponseServerDto<string>.Error(ex.Message, 404));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ResponseServerDto<string>.Error(ex.Message, 500));
-            }
+            _eventService.Delete(id);
+            return Ok(ResponseServerDto<string>.Success("Событие успешно удалено", 200));
         }
 
         [HttpPatch("{id:guid}/soft-delete")]
         public ActionResult<ResponseServerDto<string>> SoftDelete(Guid id)
         {
-            try
-            {
-                _eventService.SoftDelete(id);
-                return Ok(ResponseServerDto<string>.Success("Событие успешно деактивировано", 200));
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ResponseServerDto<string>.Error(ex.Message, 404));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ResponseServerDto<string>.Error(ex.Message, 500));
-            }
+            _eventService.SoftDelete(id);
+            return Ok(ResponseServerDto<string>.Success("Событие успешно деактивировано", 200));
         }
     }
 }
