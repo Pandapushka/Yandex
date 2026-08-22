c44e2d13c32214bf1a54bf5126a5ad58ed9d35ce
Никита Андреев <andreev.nik.d@yandex.ru>
2026-08-22 12:42:05 +0300

    добавил генератор jwt и хешер для пароля

---
+++ WebApiEvent.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiEvent.Domain.Enums;
+
+namespace WebApiEvent.Application.Interfaces
+{
+    public interface IJwtTokenGenerator
+    {
+        string Generate(Guid userId, string login, UserRole role);
+    }
+}
